37b9351deddffefe09144233aef90e2e8a2e207a
Nahum Texeira <nahumtexeira47@gmail.com>
2023-08-10 09:43:33 -0300

    actualizacion

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,33 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.getElementById("regBtn").addEventListener("click",validarfor)
+
+function validarfor(){
+
+    var data = document.getElementsByClassName("form-control");
+    
+
+    for (var i=0; i< data.lenght; i++){
+        if (data [i].value === " "){showAlertError() 
+            return false}
+    }
+
+    var contraseña = document.getElementById("password1")
+    var contraseña2 = document.getElementById("password2")
+
+ if(contraseña.value.lenght < 6){
+   showAlertError();
+   return false
+ }
+
+ if(contraseña2.value.lenght < 6){
+    showAlertError()
+    return false
+ }
+
+ showAlertSuccess()
+ return true
+}
+
