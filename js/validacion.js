2ea93672a6bcdce7ad89b5606da00fdfea393d32
s-cuevas <santicuevasferrer@gmail.com>
2023-08-10 10:17:34 -0300

    actualizacionfinal
    agregue ; y subi todas las variables apra que quede mas ordenado

--- js/validacion.js
+++ js/validacion.js
@@ -9,39 +9,37 @@ function showAlertError() {
 document.getElementById("regBtn").addEventListener("click",validarfor)
 
 function validarfor(){
-
     var data = document.getElementsByClassName("form-control");
-    
-
-    for (var i=0; i< data.length; i++){
-        if (data [i].value === " "){showAlertError() 
-            return false}
-    }
-
     var contraseña = document.getElementById("password1")
     var contraseña2 = document.getElementById("password2")
+    var checkbox = document.getElementById("terminos").checked
 
+    for (var i=0; i< data.length; i++){
+        if (data [i].value === ""){
+            showAlertError();
+            return false;
+        }
+    }
+   
  if(contraseña.value.length < 6){
    showAlertError();
-   return false
+   return false;
  }
 
  if(contraseña2.value.length < 6){
-    showAlertError()
-    return false
+    showAlertError();
+    return false;
  }
 
 
  if(contraseña.value !== contraseña2.value){
-    showAlertError()
-    return false
- }
-
- var checkbox = document.getElementById("terminos").checked
+    showAlertError();
+    return false;
+ } 
 
  if(!checkbox){
-    showAlertError()
-    return false
+    showAlertError();
+    return false;
  }
 
  showAlertSuccess()
